5748183baa252b56481adfc0ad1cc3b75ef3a4c3
fin <whittaker.fin@gmail.com>
2022-03-09 13:45:22 +0000

    reformatting

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 
 const init = () => {
-  mongoose.set("debug", true);
+    mongoose.set("debug", true);
 
-  mongoose
-    .connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-    })
-    .catch((err) => {
-      console.error("error: " + err.stack);
-      process.exit(1);
+    mongoose
+        .connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+        })
+        .catch((err) => {
+            console.error("error: " + err.stack);
+            process.exit(1);
+        });
+    mongoose.connection.on("open", () => {
+        console.log("Connected to DB");
     });
-  mongoose.connection.on("open", () => {
-    console.log("Connected to DB");
-  });
 };
 
 mongoose.Promise = global.Promise;
