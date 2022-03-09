9a8046717d3228825d5112141bc2006b4b54eed7
fin <whittaker.fin@gmail.com>
2022-03-09 13:45:57 +0000

    adding register and login

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 require("./db")();
 
 const {
-  getAllProjects,
-  getSingleProject,
-  addProject,
+    getAllProjects,
+    getSingleProject,
+    addProject,
 } = require("./controllers/project_controller.js");
+const {
+    register,
+    login,
+    loginRequired
+} = require("./controllers/user_controller.js");
+
+
 
 const port = process.env.PORT || 3000;
 
@@ -16,14 +24,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-////////////////////Routes////////////////////
+app.use((req, res, next) => {
+    if (
+        req.headers &&
+        req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'Bearer'
+    ) {
+        jwt.verify(req.headers.authorization.split(' ')[1], 'login_example', (err, decode) => {
+            if (err) req.user = undefined
+            req.user = decode
+            next()
+        })
+    } else {
+        req.user = undefined
+        next()
+    }
+});
+
+////////////////////ROUTES////////////////////
 
 app.get("/projects", getAllProjects);
-app.get("/projects/:id", getSingleProject);
-app.post("/projects", addProject);
+app.get("/projects/:id", loginRequired, getSingleProject);
+app.post("/projects", loginRequired, addProject);
+
+app.post("/register", register);
+app.post("/login", login);
 
 /////////////////////////////////////////////
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
